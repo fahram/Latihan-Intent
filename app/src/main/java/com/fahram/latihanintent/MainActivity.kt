eb3a965ac8b52c25a4a9f333ca985db3f5bb0741
Khaidir Fahram <khaidir@fahram.com>
2022-03-03 20:25:28 +0700

    Move Activity (Explicit)

--- app/src/main/java/com/fahram/latihanintent/MainActivity.kt
+++ app/src/main/java/com/fahram/latihanintent/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.fahram.latihanintent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var btnMove : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnMove = findViewById(R.id.btn_move)
+        btnMove.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_move -> {
+                startActivity(Intent(this@MainActivity, MoveActivity::class.java))
+            }
+        }
     }
 }
